f11967734550249f60034131d529991a5c77de95
42mrnobody42-alt <42mrnobody42@gmail.com>
2026-08-07 13:01:05 -0500

    consultas sql de las tablas creadas

---
+++ docs/sql/consultasBasicas.sql
@@ -0,0 +1,20 @@
+select * from investment_tracker.plataformas;
+
+select * from investment_tracker.comisiones;
+
+select * from investment_tracker.schema_version;
+
+select * from investment_tracker.calculos_hist;
+
+select * from investment_tracker.comisiones;
+
+select * from investment_tracker.roles;
+
+select * from investment_tracker.transacciones;
+
+select * from investment_tracker.usuario_roles;
+
+select * from investment_tracker.monedas;
+
+select * from investment_tracker.usuarios;
+
